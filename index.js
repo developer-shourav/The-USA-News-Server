3cc8fd23a8c36d0790e327f08dff419316a057b2
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-22 21:27:31 +0600

    data send on request complete

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000 ;
 
+const catagories = require('./data/categories.json');
 
 app.get('/', (req, res) => {
   res.send('The USA News is running')
 });
+
+app.get('/catagories', (req, res) => {
+    res.send(catagories)
+})
 
 app.listen(port , () => {
     console.log(`The USA New is running on Port: ${port}` );
