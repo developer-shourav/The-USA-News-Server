eb502d17e763fadb614d4c27a1cca76e02f709b0
developer-shourav <mrinmoyraj823@gmail.com>
2023-04-22 21:37:58 +0600

    cors added

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 5000 ;
 
 const catagories = require('./data/categories.json');
+
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('The USA News is running')
